4d7fcd8b236c1a7fa3de68a9469339af3c8229cd
MuhammetInceee <muhammetince@oxogames.net>
2023-01-09 15:36:27 +0300

    push

--- Assets/OXO/Scripts/_Scripts/Bullet/BulletCollisionManager.cs
+++ Assets/OXO/Scripts/_Scripts/Bullet/BulletCollisionManager.cs
@@ -8,19 +8,17 @@ namespace RigMan.Bullet
     public class BulletCollisionManager : MonoBehaviour
     {
         private float _damage;
-        private void OnCollisionEnter(Collision collision)
-        {
-            if (collision.collider.TryGetComponent(out OnDamageable onDamageable))
-            {
-                onDamageable.GetDamage(_damage);
-            }
-        }
 
         private void OnTriggerEnter(Collider other)
         {
             if (other.TryGetComponent(out TargetManager targetManager))
             {
                 _damage = targetManager.damage;
+            }
+            
+            if (other.TryGetComponent(out OnDamageable onDamageable))
+            {
+                onDamageable.GetDamage(_damage);
             }
         }
     }
